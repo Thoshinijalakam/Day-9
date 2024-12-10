0efb8a02755c1236ace8e65e6fff4d2431b28111
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-10 12:03:52 +0530

    Add files via upload

---
+++ Nextnextline.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Nextnextline{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(a);
+        String b=sc.nextLine();
+        System.out.println(b);
+    }
+    
+}
+/*  hello thoshini
+hello
+ thoshini */
