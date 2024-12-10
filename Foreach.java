e3925e6a815f420e535edbe57a156624a67ecad2
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-10 12:04:11 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Foreach {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","orange","pineapple"};
+        for(String b:a)
+        System.out.print(b+",");
+    }
+    
+}
+/*  Apple,orange,pineapple, */
