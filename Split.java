e650a4e8c871033bce4e1b63ee29308227a34acf
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-10 12:03:06 +0530

    Add files via upload

---
+++ Split.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Split {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String sp[]=a.split(" ");
+        System.out.println(Arrays.toString(sp));
+
+    }
+    
+}
+/*   thoshini is a good girl
+[thoshini, is, a, good, girl] */
