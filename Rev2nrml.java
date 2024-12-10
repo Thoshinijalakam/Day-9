d7de025c4fa456a57a67102940bc2180423e1048
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-10 12:03:21 +0530

    Add files via upload

---
+++ Rev2nrml.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Rev2nrml {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String res=" ";
+        for(int i=0;i<a.length();i++)
+        res=a.charAt(i)+res;
+        System.out.println(res);
+    }
+    
+}
+/*  thoshini
+inihsoht  */
